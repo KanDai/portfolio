f39735c23aab6c45f061f4208bb47503d6e2e7f2
Daichi Kanke <kanke@tam-tam.co.jp>
2020-01-20 23:14:44 +0900

    first commit

---
+++ src/templates/WorksDetail.vue
@@ -0,0 +1,28 @@
+<template>
+  <Layout>
+
+    <h1>{{ $page.work.title }}</h1>
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
+    </p>
+
+  </Layout>
+</template>
+
+<page-query>
+query work($id: ID!) {
+  work: contentfulWorks(id: $id) {
+    title
+    url
+  }
+}
+</page-query>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
